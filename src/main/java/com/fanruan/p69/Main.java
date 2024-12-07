048ada31055c5002d8ff18f43f5d1c7596998481
Anner <anner@fanruan.com>
2024-12-07 13:32:32 +0800

    新增两道新的题目，懂得都懂
    Signed-off-by: Anner <anner@fanruan.com>

---
+++ src/main/java/com/fanruan/p69/Main.java
@@ -0,0 +1,14 @@
+package com.fanruan.p69;
+
+
+/**
+ * 一本书里有100万个单词，判断其中可能写错的单词有哪些，并给出可能正确的拼写,评估其空间复杂度和时间复杂度
+ * <p>
+ * <a href="https://judge.fineres.com/problem/69">#69. 单词纠错</a>
+ *
+ * @author Anner
+ * @since 12.0
+ * Created on 2024/12/7
+ */
+public class Main {
+}
---
+++ src/main/java/com/fanruan/p70/Main.java
@@ -0,0 +1,14 @@
+package com.fanruan.p70;
+
+
+/**
+ * 有A,B,C三个字段,A作为行维度，B作为列维度,C为指标作求和计算，评估其空间复杂度和时间复杂度
+ *
+ * <a href="https://judge.fineres.com/problem/70">#70. 交叉表</a>
+ *
+ * @author Anner
+ * @since 12.0
+ * Created on 2024/12/7
+ */
+public class Main {
+}
